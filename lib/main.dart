6919e8532507eaf7330bd6c6eb803801096f09b5
Kyle Debro <debro@umich.edu>
2025-07-17 13:27:41 -0400

    Integrate Firebase into ZenSort by adding firebase_core dependency and initializing Firebase in main.dart. Update Android and macOS configurations for Firebase support, and register Firebase plugins in generated files.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:zensort/theme.dart';
+import 'package:firebase_core/firebase_core.dart'; 
+import 'firebase_options.dart'; 
 
-void main() {
+void main() async { 
+  WidgetsFlutterBinding.ensureInitialized(); 
+  await Firebase.initializeApp( 
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
