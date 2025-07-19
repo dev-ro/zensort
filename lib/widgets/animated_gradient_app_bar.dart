eaa9496b57953f44db9acbaf787da65a00f4251d
Kyle Debro <debro@umich.edu>
2025-07-19 17:17:10 -0400

    Update dependencies and add AnimatedGradientAppBar widget
    - Upgraded email_validator to version 3.0.0 and updated flutter_svg, firebase_core, cloud_functions, and flutter_lints to their latest versions.
    - Introduced a new AnimatedGradientAppBar widget for enhanced UI with a gradient animation.
    - Modified web index.html and manifest.json for improved branding and user experience.

---
+++ lib/widgets/animated_gradient_app_bar.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:zensort/theme.dart';
+
+class AnimatedGradientAppBar extends StatefulWidget
+    implements PreferredSizeWidget {
+  const AnimatedGradientAppBar({super.key});
+
+  @override
+  State<AnimatedGradientAppBar> createState() => _AnimatedGradientAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _AnimatedGradientAppBarState extends State<AnimatedGradientAppBar>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 10),
+    )..repeat(reverse: true);
+
+    _animation = Tween<double>(
+      begin: -1.0,
+      end: 1.0,
+    ).animate(CurvedAnimation(parent: _controller, curve: Curves.easeInOut));
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animation,
+      builder: (context, child) {
+        return AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                colors: ZenSortTheme.primaryGradient.colors
+                    .map((color) => color.withOpacity(0.8))
+                    .toList(),
+                begin: Alignment(_animation.value, -1.0),
+                end: Alignment(-_animation.value, 1.0),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
