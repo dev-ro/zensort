1b2ec74bdd4671f53c6bb691a1bcb59dd33754bb
Kyle Debro <debro@umich.edu>
2025-07-23 01:41:00 -0400

    clean: remove unused cloud_firestore import from repository interface
    - Remove unused DocumentSnapshot import now that pagination is eliminated
    - Clean up repository interface to only include necessary dependencies

--- lib/features/youtube/domain/repositories/youtube_repository.dart
+++ lib/features/youtube/domain/repositories/youtube_repository.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:zensort/features/youtube/domain/entities/liked_video.dart';
 import 'package:zensort/features/youtube/domain/entities/sync_progress.dart';
 
