287f4679ee24d824ac8399981cfb371fe753b4fb
Kyle Debro <debro@umich.edu>
2025-10-09 19:19:21 -0400

    feat(youtube): Expose watchEmbeddingProgress() in repository interface

--- lib/features/youtube/domain/repositories/youtube_repository.dart
+++ lib/features/youtube/domain/repositories/youtube_repository.dart
@@ -1,11 +1,13 @@
 import 'package:zensort/features/youtube/domain/entities/liked_video.dart';
 import 'package:zensort/features/youtube/domain/entities/liked_videos_page.dart';
 import 'package:zensort/features/youtube/domain/entities/sync_progress.dart';
+import 'package:zensort/features/youtube/domain/entities/embedding_progress.dart';
 
 abstract class YoutubeRepository {
   Future<void> syncLikedVideos();
   Stream<SyncProgress> getSyncProgressStream();
   Stream<List<LikedVideo>> watchLikedVideos();
+  Stream<EmbeddingProgress> watchEmbeddingProgress();
 
   Future<int> fetchRemoteLikedVideosTotal();
   Future<int> fetchLocalLikedVideosCount();
