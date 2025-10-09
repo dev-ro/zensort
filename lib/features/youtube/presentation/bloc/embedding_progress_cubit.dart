a7d2739ea9d0e89b00a02df3551a37acc9491196
Kyle Debro <debro@umich.edu>
2025-10-09 19:20:23 -0400

    feat(youtube): Add EmbeddingProgressCubit to stream real-time embedding progress

---
+++ lib/features/youtube/presentation/bloc/embedding_progress_cubit.dart
@@ -0,0 +1,23 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:zensort/features/youtube/domain/entities/embedding_progress.dart';
+import 'package:zensort/features/youtube/domain/repositories/youtube_repository.dart';
+
+class EmbeddingProgressCubit extends Cubit<EmbeddingProgress> {
+  final YoutubeRepository _repository;
+  StreamSubscription<EmbeddingProgress>? _sub;
+
+  EmbeddingProgressCubit(this._repository)
+      : super(const EmbeddingProgress()) {
+    _sub = _repository.watchEmbeddingProgress().listen(emit);
+  }
+
+  @override
+  Future<void> close() {
+    _sub?.cancel();
+    return super.close();
+  }
+}
+
+
